0ede3317c626fc807091a62341af7561b479e227
rhonesko <rafael.honesko1305@gmail.com>
2022-07-15 14:00:46 -0300

    Primeiro Commit - Projeto Conversor de moedas

---
+++ scripts.js
@@ -0,0 +1,51 @@
+const button = document.getElementById("convert-button");
+const select = document.getElementById("currency-select");
+
+const dolar = 5.2;
+const euro = 5.4;
+
+const convertValues = () => {
+  const inputReais = document.getElementById("input-real").value;
+  const realValueText = document.getElementById("real-value-text");
+  const dolarValueText = document.getElementById("dolar-value-text");
+
+  //realValueText.innerHTML = inputReais
+  realValueText.innerHTML = new Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputReais);
+
+  if (select.value === "US$ Dólar Americano") {
+    dolarValueText.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputReais / dolar);
+  }
+
+  if (select.value === "€ Euro") {
+    dolarValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputReais / euro);
+  }
+};
+
+changeCurrency = () => {
+  const currencyName = document.getElementById("currency-name");
+  const currencyImg = document.getElementById("currency-img");
+
+  if (select.value === "US$ Dólar Americano") {
+    currencyName.innerHTML = "Dólar Americano";
+    currencyImg.src = "assets/estados-unidos (1) 1.png";
+  }
+
+  if (select.value === "€ Euro") {
+    currencyName.innerHTML = "Euro";
+    currencyImg.src = "assets/EURO.png";
+  }
+
+  convertValues()
+};
+
+button.addEventListener("click", convertValues);
+select.addEventListener("change", changeCurrency);
